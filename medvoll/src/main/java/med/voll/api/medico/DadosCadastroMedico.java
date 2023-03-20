60a82d3447ddc9332387822cc7ad07a0cba7a4fc
nickunger99 <nicolas.unger@yahoo.com.br>
2023-03-20 08:51:12 -0300

    nova migration

--- medvoll/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ medvoll/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -10,7 +11,10 @@ public record DadosCadastroMedico(
         @NotBlank
         String nome,
         @NotBlank
+        @Email
         String email,
+        @NotBlank
+        String telefone,
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
