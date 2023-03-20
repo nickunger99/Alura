79eec7b71550e922ea0020841dcdcc31053bf20b
nickunger99 <nicolas.unger@yahoo.com.br>
2023-03-20 11:11:28 -0300

    get pageable

--- medvoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ medvoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,8 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,9 +27,14 @@ public class MedicoController {
        repository.save(new Medico(dados));
     }
 
+//    @GetMapping
+//    public List<DadosListagemMedico> listar(){
+//    return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+//    }
+
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-    return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable pageable){
+        return repository.findAll(pageable).map(DadosListagemMedico::new);
     }
 
 }
