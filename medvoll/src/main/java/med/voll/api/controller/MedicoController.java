1344d6b9b046e43c2b7c25252126c5d12083412b
nickunger99 <nicolas.unger@yahoo.com.br>
2023-03-21 09:47:34 -0300

    delete logico medicos

--- medvoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ medvoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -32,7 +32,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable pageable) {
-        return repository.findAll(pageable).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(pageable).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -40,6 +40,13 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 
 
--- medvoll/src/main/java/med/voll/api/medico/Medico.java
+++ medvoll/src/main/java/med/voll/api/medico/Medico.java
@@ -26,7 +26,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    public Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -40,5 +43,9 @@ public class Medico {
         if(dados.nome() != null) this.nome = dados.nome();
         if(dados.telefone()!= null) this.telefone = dados.telefone();
         if(dados.endereco() != null) this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
