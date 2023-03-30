b806de31af774c660dc537a4a7bee55804e300d7
nickunger99 <nicolas.unger@yahoo.com.br>
2023-03-30 09:42:25 -0300

    JWT no swagger

--- medvoll/src/main/java/med/voll/api/controller/ConsultaController.java
+++ medvoll/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
