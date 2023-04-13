72b955c7039e7e8ef6bc793e921d226fa711d73e
nickunger99 <nicolas.unger@yahoo.com.br>
2023-04-13 14:46:45 -0300

    JsonAlias e JsonIgnoreProperties

--- medvoll/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ medvoll/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -1,16 +1,21 @@
 package med.voll.api.domain.consulta;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosAgendamentoConsulta(
 
+        @JsonAlias({"medico_id", "medicoid", "id_medico"})
         Long idMedico,
 
         @NotNull
+        @JsonAlias({"paciente_id", "pacienteid", "id_paciente"})
         Long idPaciente,
         @NotNull
         @Future
